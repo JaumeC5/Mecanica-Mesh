b24132a7378efc231597f992e9d8799647847045
huiyu <huiyu@LAPTOP-E2V8KVPN>
2017-03-14 19:22:12 +0100

    Random Sphere

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,17 +1,27 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_glfw_gl3.h>
 #include <iostream>
+#include <cstdio>
+#include <glm\gtc\matrix_transform.hpp>
+#include <time.h>
+#include <ctime>
 bool show_test_window = false;
 
-
+float sphereX; //z
+float sphereY;
+float sphereZ;
 namespace ClothMesh {
 	extern const int numCols;
 	extern const int numRows;
 	extern const int numVerts;
-
-
 	extern void updateClothMesh(float* array_data);
 
+}
+namespace Sphere {
+	extern void setupSphere(glm::vec3 pos = glm::vec3(5.f, 1.f, 0.f), float radius = 1.f);
+	extern void cleanupSphere();
+	extern void updateSphere(glm::vec3 pos, float radius = 1.f);
+	extern void drawSphere();
 }
 void GUI() {
 	{	//FrameRate
@@ -27,7 +37,9 @@ void GUI() {
 	}
 }
 float arr[14 * 18 * 3];
+
 void PhysicsInit() {
+	srand(time(NULL));
 	int counterX = 0;
 	int counterY = 0;
 	int counter = 0;
@@ -42,12 +54,20 @@ void PhysicsInit() {
 
 		//TODO
 	}
-	ClothMesh::updateClothMesh(arr);
+	sphereX = (((float)rand() / RAND_MAX) *4.5 - 4.5); //z
+	sphereY = (((float)rand() / RAND_MAX) *0.5 + 5.f);
+	sphereZ = (((float)rand() / RAND_MAX) *4.5f - 4.5f);
+	Sphere::updateSphere(glm::vec3(sphereX, sphereY, sphereZ), 1.f);
+	//ClothMesh::updateClothMesh(arr);
 }
+
 void PhysicsUpdate(float dt) {
 	//TODO	}
+	std::srand(std::time(NULL));
 
-
+	float counter = 0;
+	
+	
 }
 void PhysicsCleanup() {
 	//TODO
