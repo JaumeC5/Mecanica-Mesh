cb15aef2b1830b9283c4827e90bc5bb4f77ca9c7
hzhou <hzhou@PB1-37>
2017-03-21 14:02:47 +0100

    Vec3 its working :D

--- src/physics.cpp
+++ src/physics.cpp
@@ -4,6 +4,7 @@
 #include <glm\gtc\matrix_transform.hpp>
 #include <time.h>
 #include <ctime>
+#include <vector>
 bool show_test_window = false;
 float xForce = 0, yForce = -9.81, zForce = 0;
 
@@ -33,33 +34,64 @@ void GUI() {
 		ImGui::ShowTestWindow(&show_test_window);
 	}
 }
-float *currMesh; // mesh array
-float *lastMesh;
-float *tempMesh;
-float *finalMesh;
-
-
-
-float counterX = 0;
-float counterY = 0;
+//float *currMesh; // mesh array
+//float *lastMesh;
+//float *tempMesh;
+//float *finalMesh;
+glm::vec3 *currMesh;	
+glm::vec3* lastMesh;
+glm::vec3 * tempMesh;
+glm::vec3 * finalMesh; 
+glm::vec3 f;
 int counter = 0;
+class spring {
+	glm::vec3 originalPos = glm::vec3();
+	//float originalPos[3];
+	glm::vec3 vel = glm::vec3();
+	//float v[3];
+	const float ke = 1;
+	const float kd = 1;
+	const float L = 0.5;
+	float f;
+public:
+	float pos[3];
+	//void Update();
+	void fuerza(spring nextNode) {
+		//f = (ke*(sqrt(pow(pos[0] - nextNode.pos[0], 2) + pow(pos[1] - nextNode.pos[1], 2) + pow(pos[2] - nextNode.pos[3], 2))-L) + kd*() )
+		originalPos = glm::vec3();
+		
+	}
+};
+
 void PhysicsInit() {
 	//gravity
 	//muelles
-	currMesh = new float[14 * 18 * 3];
-	lastMesh = new float[14 * 18 * 3];
-	tempMesh = new float[14 * 18 * 3];
-	finalMesh = new float[14 * 18 * 3];
-		float counter = 0;
+	//currMesh = new float[14 * 18 * 3];
+	//lastMesh = new float[14 * 18 * 3];
+	//tempMesh = new float[14 * 18 * 3];
+	//finalMesh = new float[14 * 18 * 3];
+	tempMesh = new glm::vec3[14 * 18];
+	finalMesh = new glm::vec3[14 * 18];
+	currMesh = new glm::vec3[14 * 18];
+	lastMesh = new glm::vec3[14 * 18];
+
 		for (int i = 0; i < ClothMesh::numCols; i++) {
-		for (int j = 0; j < ClothMesh::numRows; j++) {
-			currMesh[(j * ClothMesh::numCols + i) * 3] = -4.5+j*0.5f; //x 
+			for (int j = 0; j < ClothMesh::numRows; j++) {
+
+				currMesh[(j * ClothMesh::numCols + i)] = glm::vec3(-4.5 + j*0.5f, 9.5, -4.5 + i*0.5f);
+
+		/*	currMesh[(j * ClothMesh::numCols + i) * 3] = -4.5+j*0.5f; //x 
 			currMesh[(j * ClothMesh::numCols + i) * 3 + 1] = 9.5; //y 
 			currMesh[(j * ClothMesh::numCols + i) * 3 + 2] = -4.5 + i*0.5f; //z
 			
+			*/
+				lastMesh[(j * ClothMesh::numCols + i)] = currMesh[(j * ClothMesh::numCols + i)];	
+				f = glm::vec3(0, -9.81, 0);
+/*			
 			lastMesh[(j * ClothMesh::numCols + i) * 3] = currMesh[(j * ClothMesh::numCols + i) * 3];
 			lastMesh[(j * ClothMesh::numCols + i) * 3 + 1] = currMesh[(j * ClothMesh::numCols + i) * 3 + 1];
-			lastMesh[(j * ClothMesh::numCols + i) * 3 + 2] = currMesh[(j * ClothMesh::numCols + i) * 3 + 2];
+			lastMesh[(j * ClothMesh::numCols + i) * 3 + 2] = currMesh[(j * ClothMesh::numCols + i) * 3 + 2];*/
+			
 		}
 		//TODO
 	}
@@ -82,27 +114,40 @@ void PhysicsUpdate(float dt) {
 	for (int i = 0; i < ClothMesh::numCols; i++) {
 		for (int j = 0; j < ClothMesh::numRows; j++) {
 			tempMesh = currMesh;
+			finalMesh[(j * ClothMesh::numCols + i)] = currMesh[(j*ClothMesh::numCols + i)] + (currMesh[(j*ClothMesh::numCols + i)] - lastMesh[(j*ClothMesh::numCols + i)]) + (f / glm::vec3(1,1,1))*(dt*dt);
+			lastMesh[(j * ClothMesh::numCols + i)] = tempMesh[(j * ClothMesh::numCols + i)];
+			currMesh[(j * ClothMesh::numCols + i)] = finalMesh[(j * ClothMesh::numCols + i)];
+		}
+	}
 
+
+/*	for (int i = 0; i < ClothMesh::numCols; i++) {
+		for (int j = 0; j < ClothMesh::numRows; j++) {
+			tempMesh = currMesh;
 			finalMesh[(j * ClothMesh::numCols + i) * 3] = currMesh[(j * ClothMesh::numCols + i) * 3] + (currMesh[(j * ClothMesh::numCols + i) * 3] - lastMesh[(j * ClothMesh::numCols + i) * 3]) + (xForce / 1)*(dt*dt);
 			finalMesh[(j * ClothMesh::numCols + i) * 3+1] = currMesh[(j * ClothMesh::numCols + i) * 3+1] + (currMesh[(j * ClothMesh::numCols + i) * 3+1] - lastMesh[(j * ClothMesh::numCols + i) * 3+1]) + (yForce / 1)*(dt*dt);
 			finalMesh[(j * ClothMesh::numCols + i) * 3+2] = currMesh[(j * ClothMesh::numCols + i) * 3+2] + (currMesh[(j * ClothMesh::numCols + i) * 3+2] - lastMesh[(j * ClothMesh::numCols + i) * 3+2]) + (zForce / 1)*(dt*dt);
+			
+			//finalMesh[(0 * ClothMesh::numCols + 0) * 3] = currMesh[(j * ClothMesh::numCols + i) * 3] = -4.5;
+			//finalMesh[(ClothMesh::numRows-1 * ClothMesh::numCols + 0) * 3] = currMesh[(j * ClothMesh::numCols + i) * 3] = -4.5;
+
 
 			lastMesh[(j * ClothMesh::numCols + i) * 3] = tempMesh[(j * ClothMesh::numCols + i) * 3];
 			lastMesh[(j * ClothMesh::numCols + i) * 3+1] = tempMesh[(j * ClothMesh::numCols + i) * 3+1];
 			lastMesh[(j * ClothMesh::numCols + i) * 3+2] = tempMesh[(j * ClothMesh::numCols + i) * 3+2];
 		
 		    currMesh[(j * ClothMesh::numCols + i) * 3] = finalMesh[(j * ClothMesh::numCols + i) * 3];
-			currMesh[(j * ClothMesh::numCols + i+1) * 3+1] = finalMesh[(j * ClothMesh::numCols + i) * 3+1]; // quitar el +1
+			currMesh[(j * ClothMesh::numCols + i) * 3+1] = finalMesh[(j * ClothMesh::numCols + i) * 3+1]; // quitar el +1
 			currMesh[(j * ClothMesh::numCols + i) * 3+2] = finalMesh[(j * ClothMesh::numCols + i) * 3+2];
-
 		}
-	}
+	}*/
 
-	ClothMesh::updateClothMesh(currMesh);
+	ClothMesh::updateClothMesh(&currMesh[0].x);
 
 
 	}
 
 void PhysicsCleanup() {
 	//TODO
+	
 }
