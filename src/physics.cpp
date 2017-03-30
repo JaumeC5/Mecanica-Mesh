b94124b99d813396216112d5bb90ff979a4032c7
Toni <Toni@LENOVO-PC>
2017-03-30 09:47:41 +0200

    Ingeniar colisions amb es plans per tal de que quan caigui, ho faci com un padaç

--- src/physics.cpp
+++ src/physics.cpp
@@ -58,29 +58,23 @@ public:
 	glm::vec3 lastPos;
 	glm::vec3 vel;
 	glm::vec3 OriginalPos;
-	glm::vec3 Force;
+	glm::vec3 Force = f;
 	int mass = 1;
 };
-void Particle::calculateForce(Particle nextP, float dist) { // la fuerza se descontrola. goodbye life
-
-
-	Force -= 9.81;//(ke*(sqrt(pow(ActualPos.x - nextP.ActualPos.x, 2) + pow(ActualPos.y - nextP.ActualPos.y, 2) + pow(ActualPos.z - nextP.ActualPos.z, 2)) - dist) + kd*(vel - nextP.vel)*(ActualPos - nextP.ActualPos) / (sqrt(pow(ActualPos.x - nextP.ActualPos.x, 2) + pow(ActualPos.y - nextP.ActualPos.y, 2) + pow(ActualPos.z - nextP.ActualPos.z, 2)))) *(ActualPos - nextP.ActualPos) / (sqrt(pow(ActualPos.x - nextP.ActualPos.x, 2) + pow(ActualPos.y - nextP.ActualPos.y, 2) + pow(ActualPos.z - nextP.ActualPos.z, 2)));
-		// Calculat forces L12 es sa distancia inicial, que varia depenen d on se trobi s altra particula. en diagonal se fa pitagoras, ect
-		//k*(ActualPos - nextP.ActualPos - (sqrt(pow(OriginalPos.x - nextP.OriginalPos.x, 2) + pow(OriginalPos.y - nextP.OriginalPos.y, 2) + pow(OriginalPos.z - nextP.OriginalPos.z, 2)))*((ActualPos - nextP.ActualPos) / sqrt(pow(ActualPos.x - nextP.ActualPos.x, 2) + pow(ActualPos.y - nextP.ActualPos.y, 2) + pow(ActualPos.z - nextP.ActualPos.z, 2))));
+void Particle::calculateForce(Particle nextP, float dist) {
+		
+	 // se iguala a sa força externa. Motius obvis
+	Force += -(ke * (glm::length(ActualPos - nextP.ActualPos)));//-(ke * (glm::length(ActualPos - nextP.ActualPos) - dist) +glm::dot(kd*(vel - nextP.vel), (ActualPos / nextP.ActualPos) / glm::length(ActualPos - nextP.ActualPos))) *(ActualPos / nextP.ActualPos) / glm::length(ActualPos - nextP.ActualPos);
+		
 }
 	Particle* parVerts;
 void PhysicsInit() {
-	//gravity
-	//muelles
-	//currMesh = new float[14 * 18 * 3];
-	//lastMesh = new float[14 * 18 * 3];
-	//tempMesh = new float[14 * 18 * 3];
-	//finalMesh = new float[14 * 18 * 3];
 	
 	tempMesh = new glm::vec3[14 * 18];
 	finalMesh = new glm::vec3[14 * 18];
 	currMesh = new glm::vec3[14 * 18];
 	lastMesh = new glm::vec3[14 * 18];
+
 	parVerts = new Particle[14 * 18];
 		for (int i = 0; i < ClothMesh::numCols; i++) {
 			for (int j = 0; j < ClothMesh::numRows; j++) {
@@ -92,12 +86,12 @@ void PhysicsInit() {
 				}
 
 
-
-					currMesh[(j * ClothMesh::numCols + i)] = glm::vec3(-4.5 + j*0.5f, 9.5, -4.5 + i*0.5f); // distancia horitzontal i vertical entre nodes = 0.5. Distancia doble = 1. Distància diagonal = sqrt(0.5*0.5 + 0.5*0.5);
-					lastMesh[(j * ClothMesh::numCols + i)] = currMesh[(j * ClothMesh::numCols + i)];
-					f = glm::vec3(0, -9.81, 0);
-					parVerts[j * ClothMesh::numCols + i].OriginalPos = currMesh[(j * ClothMesh::numCols + i)];
-		}
+				currMesh[(j * ClothMesh::numCols + i)] = glm::vec3(-4.5 + j*0.5f, 9.5, -4.5 + i*0.5f); // distancia horitzontal i vertical entre nodes = 0.5. Distancia doble = 1. Distància diagonal = sqrt(0.5*0.5 + 0.5*0.5);
+				f = glm::vec3(0, -9.81, 0); // força externa, sa gravetat
+				parVerts[j * ClothMesh::numCols + i].OriginalPos = currMesh[(j * ClothMesh::numCols + i)];
+				parVerts[j * ClothMesh::numCols + i].lastPos = lastMesh[(j * ClothMesh::numCols + i)];
+				parVerts[j * ClothMesh::numCols + i].Force = glm::vec3(0, 0, 0);
+			}
 	}
 	//ClothMesh::updateClothMesh(currMesh);
 }
@@ -117,85 +111,95 @@ void PhysicsUpdate(float dt) {
 	//position update
 	for (int i = 0; i < ClothMesh::numCols; i++) {
 		for (int j = 0; j < ClothMesh::numRows; j++) {
-			tempMesh = currMesh;
 			
-
-
-			if(i < ClothMesh::numCols){ // fuerza a la derecha
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + i + 1)], 0.5f);
+			if(i < ClothMesh::numCols-1){ // fuerza a la derecha
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + (i + 1))], 0.5f);
 			}
-
+			
 			if (i > 0) { // fuerza a la izquierda
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + i - 1)], 0.5f);
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + (i - 1))], 0.5f);
 			}
 
-			if (i < ClothMesh::numCols-1) {
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + i + 2)], 1);
+			if (i < ClothMesh::numCols-2) { // fuerza a las dos de la derecha
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + (i + 2))], 1);
 			}
 
-			if (i > 1) {
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + i - 2)], 1);
+			if (i > 1) {// fuerza a las dos de la izquierda
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j * ClothMesh::numCols + (i - 2))], 1);
 			}
 
-			if (j < ClothMesh::numRows) {
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j + 1 * ClothMesh::numCols + i)], 0.5);
+			if (j < ClothMesh::numRows-1) {
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j + 1) * ClothMesh::numCols + i)], 0.5);
 			}
 
 			if (j > 0) {
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j - 1 * ClothMesh::numCols + i)], 0.5);
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j - 1) * ClothMesh::numCols + i)], 0.5);
 			}
 			
 			
-			if (j < ClothMesh::numRows-1) {
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j + 2* ClothMesh::numCols + i)], 1);
+			if (j < ClothMesh::numRows-2) {
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j + 2)* ClothMesh::numCols + i)], 1);
 			}
 			
 			if (j > 1) {
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j - 2 * ClothMesh::numCols + i)], 1);
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j - 2) * ClothMesh::numCols + i)], 1);
 			}
 			
-			if (i < ClothMesh::numCols && j < ClothMesh::numRows) { // diagonal derecha abajo
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j + 1 * ClothMesh::numCols + i + 1)], 0.707f);
+			if (i < ClothMesh::numCols-1 && j < ClothMesh::numRows-1) { // diagonal derecha abajo
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j + 1) * ClothMesh::numCols + (i + 1))], 0.707f);
 			}
 
-			if (i < ClothMesh::numCols && j > 0) { // diagonal derecha arriba
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j - 1 * ClothMesh::numCols + i + 1)], 0.707f);
+			if (i < ClothMesh::numCols-1 && j > 0) { // diagonal derecha arriba
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j - 1) * ClothMesh::numCols + (i + 1))], 0.707f);
 			}
 
 			if (i > 0 && j > 0) { // diagonal izquierda arriba
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j - 1 * ClothMesh::numCols + i - 1)], 0.707f);
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j - 1) * ClothMesh::numCols + (i - 1))], 0.707f);
 			}
 
-			if (i > 0 && j < ClothMesh::numRows) { // diagonal izquierda abajo
-				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[(j + 1 * ClothMesh::numCols + i - 1)], 0.707f);
+			if (i > 0 && j < ClothMesh::numRows-1) { // diagonal izquierda abajo
+				parVerts[(j * ClothMesh::numCols + i)].calculateForce(parVerts[((j + 1) * ClothMesh::numCols + (i - 1))], 0.707f);
 			}
+
+			tempMesh[(j * ClothMesh::numCols + i)] = currMesh[(j * ClothMesh::numCols + i)];
+
 
 			if (i == 0 && j == 0) {
 
 				currMesh[(j * ClothMesh::numCols + i)] = glm::vec3(-4.5 + 0.5f, 9.5, -4.5 + 0.5f);
-				parVerts[(j * ClothMesh::numCols + i)].ActualPos = currMesh[(j * ClothMesh::numCols + i)];
-				parVerts[(j * ClothMesh::numCols + i)].vel = (parVerts[(j * ClothMesh::numCols + i)].ActualPos - parVerts[(j * ClothMesh::numCols + i)].lastPos) / dt;
+				
 
 			}
 			else if (i == ClothMesh::numCols - 1 && j == 0) {
 
 				currMesh[(j * ClothMesh::numCols + i)] = glm::vec3(-4.5 + 0.5f, 9.5, 1.f);
-				parVerts[(j * ClothMesh::numCols + i)].ActualPos = currMesh[(j * ClothMesh::numCols + i)];
-				parVerts[(j * ClothMesh::numCols + i)].vel = (parVerts[(j * ClothMesh::numCols + i)].ActualPos - parVerts[(j * ClothMesh::numCols + i)].lastPos) / dt;
+				
 
 			}
 
+			else {
 
-			finalMesh[(j * ClothMesh::numCols + i)] = currMesh[(j*ClothMesh::numCols + i)] + (currMesh[(j*ClothMesh::numCols + i)] - lastMesh[(j*ClothMesh::numCols + i)]) + (f)*(dt*dt);
-			lastMesh[(j * ClothMesh::numCols + i)] = tempMesh[(j * ClothMesh::numCols + i)];
-			currMesh[(j * ClothMesh::numCols + i)] = finalMesh[(j * ClothMesh::numCols + i)];
-			parVerts[(j * ClothMesh::numCols + i)].lastPos = lastMesh[(j * ClothMesh::numCols + i)];
 
-			parVerts[(j * ClothMesh::numCols + i)].ActualPos = currMesh[(j * ClothMesh::numCols + i)];
-			parVerts[(j * ClothMesh::numCols + i)].vel = (parVerts[(j * ClothMesh::numCols + i)].ActualPos - parVerts[(j * ClothMesh::numCols + i)].lastPos) / dt;
+				lastMesh[(j * ClothMesh::numCols + i)] = tempMesh[(j * ClothMesh::numCols + i)];
+				//cout << "/////////////" << endl;
+				//cout << lastMesh[0].x << endl; //aixo no toca donar lo mateix que currMesh, ja que lastMesh se suposa que emmagametza sa posicio anterior
+				//cout << currMesh[0].x << endl;
+
+
+				finalMesh[(j * ClothMesh::numCols + i)] = currMesh[(j*ClothMesh::numCols + i)] + (currMesh[(j*ClothMesh::numCols + i)] - lastMesh[(j*ClothMesh::numCols + i)]) + ((parVerts[(j*ClothMesh::numCols + i)].Force))*(dt*dt);
+				currMesh[(j * ClothMesh::numCols + i)] = finalMesh[(j * ClothMesh::numCols + i)];
+
+				parVerts[(j * ClothMesh::numCols + i)].lastPos = lastMesh[(j * ClothMesh::numCols + i)];
+				parVerts[(j * ClothMesh::numCols + i)].ActualPos = currMesh[(j * ClothMesh::numCols + i)];
+
+				parVerts[(j * ClothMesh::numCols + i)].vel = (parVerts[(j * ClothMesh::numCols + i)].ActualPos - parVerts[(j * ClothMesh::numCols + i)].lastPos) / dt;
+			}
 		}
 	}
-	cout << currMesh[1].y << " " << lastMesh[1].y << endl;
+	cout << "/////////////" << endl;
+	cout << currMesh[1].x << " " <<lastMesh[1].x << endl;
+	cout << parVerts[1].ActualPos.x <<" "  << parVerts[1].lastPos.x<< endl;
+	//cout << "/////////////" << endl;
 	ClothMesh::updateClothMesh(&currMesh[0].x);
 
 
